18597d374b7381e2032e6a896fc3f971ed4d67da
Sparsh <sparsh.chadha46@gmail.com>
2023-04-17 17:43:19 +0530

    migration complete

--- app/src/main/java/com/example/roomdemo/database/DatabaseMigration2To3.kt
+++ app/src/main/java/com/example/roomdemo/database/DatabaseMigration2To3.kt
@@ -6,6 +6,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 class DatabaseMigration2To3 : Migration(2, 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
         // Add the new "hehe" column to the "users" table.
-        database.execSQL("ALTER TABLE users ADD COLUMN hehe INTEGER NOT NULL DEFAULT 1")
+        database.execSQL("ALTER TABLE users ADD COLUMN hehe INTEGER NOT NULL DEFAULT 1") // use 1 or 0 for boolean always, not true or false
     }
 }
